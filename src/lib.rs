fdac4028128ac1f2fb2b920768d4b5bea8876e4b
Ps <sexypistolcs@gmail.com>
2020-09-06 08:50:34 +1000

    Added unit tests for the window screen

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,22 @@
 pub mod window;
 
 /// Unit testing for the window module
-
 /// all the unit test for the window module
 #[cfg(test)]
 mod tests {
+    use super::*;
 
-    ///random testing 
+    /// testing if the error is returned when input window size larger than 800
     #[test]
-    fn check() {
-        assert_eq!("something", "something");
+    fn should_not_create_large_window_size() {
+        let display_window = window::WindowDisplay::new(900, 900).is_err(); //creates the new instance of the display window
+        assert!(display_window);
     }
+    
+    /// testing when the window size if in the given margin
+    #[test]
+    fn should_create_right_size_window() {
+        assert!(!(window::WindowDisplay::new(700, 700).is_err()));
+    }
+
 }
