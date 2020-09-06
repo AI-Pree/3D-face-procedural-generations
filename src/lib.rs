4f0212e112a27bc9e358819fbac1b60ed6aa916b
Ps <sexypistolcs@gmail.com>
2020-09-06 10:17:41 +1000

    unit test for spawning new window using sdl2

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,13 @@ pub mod window;
 #[cfg(test)]
 mod tests {
     use super::*;
+   
+    /// testing when the window size is in the given margin
+    #[test]
+    fn should_create_right_size_window() {
+        let display_window = window::WindowDisplay::new(700, 700).is_err();
+        assert!(!display_window);
+    } 
 
     /// testing if the error is returned when input window size larger than 800
     #[test]
@@ -14,10 +21,11 @@ mod tests {
         assert!(display_window);
     }
     
-    /// testing when the window size if in the given margin
+    /// testing if the new window is spawned
     #[test]
-    fn should_create_right_size_window() {
-        assert!(!(window::WindowDisplay::new(700, 700).is_err()));
+    fn new_window_has_spawned() {
+        let display_window = window::WindowDisplay::new(400, 400).unwrap(); //creates the new instance of the display window
+        assert!(!display_window.spawn_window().is_err());
     }
-
+   
 }
